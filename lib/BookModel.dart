630964d10649f8ced69c31ed107699291054566f
mohmead mahdy <mohmedmahdy714@gmail.com>
2024-01-31 16:46:38 +0200

    add BooksModel

--- lib/BookModel.dart
+++ lib/BookModel.dart
@@ -1,14 +1,5 @@
-/// kind : "books#volume"
-/// id : "gL34DwAAQBAJ"
-/// etag : "UrGgIBBJfZg"
-/// selfLink : "https://www.googleapis.com/books/v1/volumes/gL34DwAAQBAJ"
-/// volumeInfo : {"title":"Concepts, Techniques, and Models of Computer Programming","authors":["Peter Van Roy","Seif Haridi"],"publisher":"MIT Press","publishedDate":"2004-02-20","description":"Teaching the science and the technology of programming as a unified discipline that shows the deep relationships between programming paradigms. This innovative text presents computer programming as a unified discipline in a way that is both practical and scientifically sound. The book focuses on techniques of lasting value and explains them precisely in terms of a simple abstract machine. The book presents all major programming paradigms in a uniform framework that shows their deep relationships and how and where to use them together. After an introduction to programming concepts, the book presents both well-known and lesser-known computation models (\"programming paradigms\"). Each model has its own set of techniques and each is included on the basis of its usefulness in practice. The general models include declarative programming, declarative concurrency, message-passing concurrency, explicit state, object-oriented programming, shared-state concurrency, and relational programming. Specialized models include graphical user interface programming, distributed programming, and constraint programming. Each model is based on its kernel language—a simple core language that consists of a small number of programmer-significant elements. The kernel languages are introduced progressively, adding concepts one by one, thus showing the deep relationships between different models. The kernel languages are defined precisely in terms of a simple abstract machine. Because a wide variety of languages and programming paradigms can be modeled by a small set of closely related kernel languages, this approach allows programmer and student to grasp the underlying unity of programming. The book has many program fragments and exercises, all of which can be run on the Mozart Programming System, an Open Source software package that features an interactive incremental development environment.","industryIdentifiers":[{"type":"ISBN_13","identifier":"9780262220699"},{"type":"ISBN_10","identifier":"0262220695"}],"readingModes":{"text":false,"image":true},"pageCount":931,"printType":"BOOK","categories":["Computers"],"maturityRating":"NOT_MATURE","allowAnonLogging":false,"contentVersion":"0.1.0.0.preview.1","panelizationSummary":{"containsEpubBubbles":false,"containsImageBubbles":false},"imageLinks":{"smallThumbnail":"http://books.google.com/books/content?id=gL34DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api","thumbnail":"http://books.google.com/books/content?id=gL34DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"},"language":"en","previewLink":"http://books.google.com.eg/books?id=gL34DwAAQBAJ&printsec=frontcover&dq=programing&hl=&cd=1&source=gbs_api","infoLink":"http://books.google.com.eg/books?id=gL34DwAAQBAJ&dq=programing&hl=&source=gbs_api","canonicalVolumeLink":"https://books.google.com/books/about/Concepts_Techniques_and_Models_of_Comput.html?hl=&id=gL34DwAAQBAJ"}
-/// saleInfo : {"country":"EG","saleability":"NOT_FOR_SALE","isEbook":false}
-/// accessInfo : {"country":"EG","viewability":"PARTIAL","embeddable":true,"publicDomain":false,"textToSpeechPermission":"ALLOWED","epub":{"isAvailable":false},"pdf":{"isAvailable":true,"acsTokenLink":"http://books.google.com.eg/books/download/Concepts_Techniques_and_Models_of_Comput-sample-pdf.acsm?id=gL34DwAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"},"webReaderLink":"http://play.google.com/books/reader?id=gL34DwAAQBAJ&hl=&source=gbs_api","accessViewStatus":"SAMPLE","quoteSharingAllowed":false}
-/// searchInfo : {"textSnippet":"This innovative text presents computer programming as a unified discipline in a way that is both practical and scientifically sound."}
-
-class BooksModel {
-  BooksModel({
+class BookModel {
+  BookModel({
     this.kind,
     this.id,
     this.etag,
@@ -19,7 +10,7 @@ class BooksModel {
     this.searchInfo,
   });
 
-  BooksModel.fromJson(dynamic json) {
+  BookModel.fromJson(dynamic json) {
     kind = json['kind'];
     id = json['id'];
     etag = json['etag'];
@@ -36,7 +27,6 @@ class BooksModel {
         ? SearchInfo.fromJson(json['searchInfo'])
         : null;
   }
-
   String kind;
   String id;
   String etag;
@@ -68,8 +58,6 @@ class BooksModel {
   }
 }
 
-/// textSnippet : "This innovative text presents computer programming as a unified discipline in a way that is both practical and scientifically sound."
-
 class SearchInfo {
   SearchInfo({
     this.textSnippet,
@@ -78,7 +66,6 @@ class SearchInfo {
   SearchInfo.fromJson(dynamic json) {
     textSnippet = json['textSnippet'];
   }
-
   String textSnippet;
 
   Map<String, dynamic> toJson() {
@@ -87,17 +74,6 @@ class SearchInfo {
     return map;
   }
 }
-
-/// country : "EG"
-/// viewability : "PARTIAL"
-/// embeddable : true
-/// publicDomain : false
-/// textToSpeechPermission : "ALLOWED"
-/// epub : {"isAvailable":false}
-/// pdf : {"isAvailable":true,"acsTokenLink":"http://books.google.com.eg/books/download/Concepts_Techniques_and_Models_of_Comput-sample-pdf.acsm?id=gL34DwAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"}
-/// webReaderLink : "http://play.google.com/books/reader?id=gL34DwAAQBAJ&hl=&source=gbs_api"
-/// accessViewStatus : "SAMPLE"
-/// quoteSharingAllowed : false
 
 class AccessInfo {
   AccessInfo({
@@ -125,7 +101,6 @@ class AccessInfo {
     accessViewStatus = json['accessViewStatus'];
     quoteSharingAllowed = json['quoteSharingAllowed'];
   }
-
   String country;
   String viewability;
   bool embeddable;
@@ -157,9 +132,6 @@ class AccessInfo {
   }
 }
 
-/// isAvailable : true
-/// acsTokenLink : "http://books.google.com.eg/books/download/Concepts_Techniques_and_Models_of_Comput-sample-pdf.acsm?id=gL34DwAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
-
 class Pdf {
   Pdf({
     this.isAvailable,
@@ -170,7 +142,6 @@ class Pdf {
     isAvailable = json['isAvailable'];
     acsTokenLink = json['acsTokenLink'];
   }
-
   bool isAvailable;
   String acsTokenLink;
 
@@ -182,8 +153,6 @@ class Pdf {
   }
 }
 
-/// isAvailable : false
-
 class Epub {
   Epub({
     this.isAvailable,
@@ -192,7 +161,6 @@ class Epub {
   Epub.fromJson(dynamic json) {
     isAvailable = json['isAvailable'];
   }
-
   bool isAvailable;
 
   Map<String, dynamic> toJson() {
@@ -201,10 +169,6 @@ class Epub {
     return map;
   }
 }
-
-/// country : "EG"
-/// saleability : "NOT_FOR_SALE"
-/// isEbook : false
 
 class SaleInfo {
   SaleInfo({
@@ -218,7 +182,6 @@ class SaleInfo {
     saleability = json['saleability'];
     isEbook = json['isEbook'];
   }
-
   String country;
   String saleability;
   bool isEbook;
@@ -231,26 +194,6 @@ class SaleInfo {
     return map;
   }
 }
-
-/// title : "Concepts, Techniques, and Models of Computer Programming"
-/// authors : ["Peter Van Roy","Seif Haridi"]
-/// publisher : "MIT Press"
-/// publishedDate : "2004-02-20"
-/// description : "Teaching the science and the technology of programming as a unified discipline that shows the deep relationships between programming paradigms. This innovative text presents computer programming as a unified discipline in a way that is both practical and scientifically sound. The book focuses on techniques of lasting value and explains them precisely in terms of a simple abstract machine. The book presents all major programming paradigms in a uniform framework that shows their deep relationships and how and where to use them together. After an introduction to programming concepts, the book presents both well-known and lesser-known computation models (\"programming paradigms\"). Each model has its own set of techniques and each is included on the basis of its usefulness in practice. The general models include declarative programming, declarative concurrency, message-passing concurrency, explicit state, object-oriented programming, shared-state concurrency, and relational programming. Specialized models include graphical user interface programming, distributed programming, and constraint programming. Each model is based on its kernel language—a simple core language that consists of a small number of programmer-significant elements. The kernel languages are introduced progressively, adding concepts one by one, thus showing the deep relationships between different models. The kernel languages are defined precisely in terms of a simple abstract machine. Because a wide variety of languages and programming paradigms can be modeled by a small set of closely related kernel languages, this approach allows programmer and student to grasp the underlying unity of programming. The book has many program fragments and exercises, all of which can be run on the Mozart Programming System, an Open Source software package that features an interactive incremental development environment."
-/// industryIdentifiers : [{"type":"ISBN_13","identifier":"9780262220699"},{"type":"ISBN_10","identifier":"0262220695"}]
-/// readingModes : {"text":false,"image":true}
-/// pageCount : 931
-/// printType : "BOOK"
-/// categories : ["Computers"]
-/// maturityRating : "NOT_MATURE"
-/// allowAnonLogging : false
-/// contentVersion : "0.1.0.0.preview.1"
-/// panelizationSummary : {"containsEpubBubbles":false,"containsImageBubbles":false}
-/// imageLinks : {"smallThumbnail":"http://books.google.com/books/content?id=gL34DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api","thumbnail":"http://books.google.com/books/content?id=gL34DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"}
-/// language : "en"
-/// previewLink : "http://books.google.com.eg/books?id=gL34DwAAQBAJ&printsec=frontcover&dq=programing&hl=&cd=1&source=gbs_api"
-/// infoLink : "http://books.google.com.eg/books?id=gL34DwAAQBAJ&dq=programing&hl=&source=gbs_api"
-/// canonicalVolumeLink : "https://books.google.com/books/about/Concepts_Techniques_and_Models_of_Comput.html?hl=&id=gL34DwAAQBAJ"
 
 class VolumeInfo {
   VolumeInfo({
@@ -308,7 +251,6 @@ class VolumeInfo {
     infoLink = json['infoLink'];
     canonicalVolumeLink = json['canonicalVolumeLink'];
   }
-
   String title;
   List<String> authors;
   String publisher;
@@ -363,9 +305,6 @@ class VolumeInfo {
   }
 }
 
-/// smallThumbnail : "http://books.google.com/books/content?id=gL34DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
-/// thumbnail : "http://books.google.com/books/content?id=gL34DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-
 class ImageLinks {
   ImageLinks({
     this.smallThumbnail,
@@ -376,7 +315,6 @@ class ImageLinks {
     smallThumbnail = json['smallThumbnail'];
     thumbnail = json['thumbnail'];
   }
-
   String smallThumbnail;
   String thumbnail;
 
@@ -388,9 +326,6 @@ class ImageLinks {
   }
 }
 
-/// containsEpubBubbles : false
-/// containsImageBubbles : false
-
 class PanelizationSummary {
   PanelizationSummary({
     this.containsEpubBubbles,
@@ -401,7 +336,6 @@ class PanelizationSummary {
     containsEpubBubbles = json['containsEpubBubbles'];
     containsImageBubbles = json['containsImageBubbles'];
   }
-
   bool containsEpubBubbles;
   bool containsImageBubbles;
 
@@ -413,9 +347,6 @@ class PanelizationSummary {
   }
 }
 
-/// text : false
-/// image : true
-
 class ReadingModes {
   ReadingModes({
     this.text,
@@ -426,7 +357,6 @@ class ReadingModes {
     text = json['text'];
     image = json['image'];
   }
-
   bool text;
   bool image;
 
@@ -438,9 +368,6 @@ class ReadingModes {
   }
 }
 
-/// type : "ISBN_13"
-/// identifier : "9780262220699"
-
 class IndustryIdentifiers {
   IndustryIdentifiers({
     this.type,
@@ -451,7 +378,6 @@ class IndustryIdentifiers {
     type = json['type'];
     identifier = json['identifier'];
   }
-
   String type;
   String identifier;
 
