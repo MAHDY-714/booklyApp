a6902fa3de1d00f0dcd7a7f6f3e781f489675c4d
mohmead mahdy <mohmedmahdy714@gmail.com>
2024-02-05 11:35:58 +0200

    add get api service in HomeRepoImplement

--- lib/features/home/data/repos/home_repo_implemnt.dart
+++ lib/features/home/data/repos/home_repo_implemnt.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: dead_code
 
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/core/utils/errors/failure.dart';
 import 'package:bookly_app/features/home/data/model/books_model/books_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
@@ -7,10 +8,26 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
 class HomeRepoImplement implements HomeRepo {
+  final endPoint =
+      'volumes?q=supject: programing&projection=full&orderBy=newest';
+  final ApiService apiService;
+
+  HomeRepoImplement(this.apiService);
   @override
-  Future<Either<Failure, List<BooksModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BooksModel>>> fetchBestSellerBooks() async {
+    try {
+      var data = await apiService.get(endPoint: endPoint);
+      //  List<BooksModel> books = [];
+      // books.add(BooksModel.fromJson(data));
+
+      List<BooksModel> books = [];
+      for (var item in data['items']) {
+        books.add(BooksModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
